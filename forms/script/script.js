f02111627e6e14d43b0c39bd429942a0bc43fe36
Hephzibah <hephzibahijeje89@gmail.com>
2024-08-01 08:54:41 -0400

    files

--- forms/script/script.js
+++ forms/script/script.js
@@ -1,3 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navbar = document.querySelector('.navbar');
+    
+    hamburger.addEventListener('click', function() {
+        navbar.classList.toggle('responsive');
+    });
+});
+
 function navigateTo(page) {
     window.location.href = page;
 }
